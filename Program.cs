5c981c09a2900bbfaefba8b93cb261aac2fe73be
boneki <60019959+boneki@users.noreply.github.com>
2020-01-31 19:00:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInputEthanAnderson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Ethan Anderson.");
+
+            Console.WriteLine("Type something and hit enter: ");
+
+            string userString;
+
+            userString = Console.ReadLine();
+
+            Console.WriteLine("You typed: " + userString);
+        }
+    }
+}
